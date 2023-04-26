e8a49bd2712949a2ff25938ac79be4b878ddf76a
Sergey Evseev <sevseev@gmail.com>
2023-04-26 03:08:38 +0300

    resolutionDropdown

---
+++ USB_Camera_Stream/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace USB_Camera_Stream
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.resolutionDropdown = new MetroFramework.Controls.MetroComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(960, 540);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // resolutionDropdown
+            // 
+            this.resolutionDropdown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.resolutionDropdown.FormattingEnabled = true;
+            this.resolutionDropdown.ItemHeight = 23;
+            this.resolutionDropdown.Location = new System.Drawing.Point(918, 523);
+            this.resolutionDropdown.Name = "resolutionDropdown";
+            this.resolutionDropdown.Size = new System.Drawing.Size(154, 29);
+            this.resolutionDropdown.TabIndex = 1;
+            this.resolutionDropdown.UseSelectable = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 672);
+            this.Controls.Add(this.resolutionDropdown);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroComboBox resolutionDropdown;
+    }
+}
+
--- USB_Camera_Stream/Form1.cs
+++ USB_Camera_Stream/Form1.cs
@@ -9,10 +9,13 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using AForge.Video;
 using AForge.Video.DirectShow;
+using MetroFramework;
+using MetroFramework.Forms;
+
 
 namespace USB_Camera_Stream
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroForm
     {
         private FilterInfoCollection videoDevices;
         private VideoCaptureDevice videoSource;
@@ -21,6 +24,9 @@ namespace USB_Camera_Stream
         public Form1()
         {
             InitializeComponent();
+            this.Style = MetroFramework.MetroColorStyle.Magenta;
+            this.Theme = MetroFramework.MetroThemeStyle.Light;
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -32,10 +38,12 @@ namespace USB_Camera_Stream
                 MessageBox.Show("No video devices found.");
                 return;
             }
-
+            //Populate the resolution dropdown list with the available resolutions of the first device
+            PopulateResolutionList(0);
 
             //Select the first device
             videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            SetResolution(0, 0); //set the default resolution of the first device
             videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
             videoSource.Start();
         }
@@ -57,7 +65,23 @@ namespace USB_Camera_Stream
             pictureBox1.Image = (System.Drawing.Image)eventArgs.Frame.Clone();
         }
 
-        
-    }//end of public partial class Form1
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // Check if the form is in full-screen mode
+            if (WindowState == FormWindowState.Maximized)
+            {
+                // Expand the PictureBox to fill the entire form
+                pictureBox1.Size = new Size(ClientSize.Width, ClientSize.Height);
+                pictureBox1.Location = new Point(0, 0);
+            }
+            else
+            {
+                // Reset the PictureBox size and location
+                pictureBox1.Size = new Size(960, 540); // or whatever size you choose
+                pictureBox1.Location = new Point((ClientSize.Width - pictureBox1.Width) / 2,
+                                                 (ClientSize.Height - pictureBox1.Height) / 2);
+            }
+        }
+    }//end of public partial class Form1  
 
 } //end of namespace USB_Camera_Stream

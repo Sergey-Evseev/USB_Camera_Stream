fff2628dabfc56ec74ff21a20d975f89c31e13a5
Sergey Evseev <sevseev@gmail.com>
2023-07-09 20:12:14 +0300

    drag form handlersdragging events

--- USB_Camera_Stream/Form1.Designer.cs
+++ USB_Camera_Stream/Form1.Designer.cs
@@ -37,14 +37,15 @@ namespace USB_Camera_Stream
             this.contrastSlider = new MetroFramework.Controls.MetroTrackBar();
             this.brightnessLabel = new MetroFramework.Controls.MetroLabel();
             this.contrastLabel = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 50);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(960, 540);
+            this.pictureBox1.Size = new System.Drawing.Size(936, 537);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -54,7 +55,7 @@ namespace USB_Camera_Stream
             this.resolutionDropdown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.resolutionDropdown.FormattingEnabled = true;
             this.resolutionDropdown.ItemHeight = 23;
-            this.resolutionDropdown.Location = new System.Drawing.Point(918, 523);
+            this.resolutionDropdown.Location = new System.Drawing.Point(731, 642);
             this.resolutionDropdown.Name = "resolutionDropdown";
             this.resolutionDropdown.Size = new System.Drawing.Size(154, 29);
             this.resolutionDropdown.TabIndex = 1;
@@ -64,7 +65,7 @@ namespace USB_Camera_Stream
             // brightnessSlider
             // 
             this.brightnessSlider.BackColor = System.Drawing.Color.Transparent;
-            this.brightnessSlider.Location = new System.Drawing.Point(92, 607);
+            this.brightnessSlider.Location = new System.Drawing.Point(23, 636);
             this.brightnessSlider.Name = "brightnessSlider";
             this.brightnessSlider.Size = new System.Drawing.Size(169, 42);
             this.brightnessSlider.TabIndex = 2;
@@ -74,7 +75,7 @@ namespace USB_Camera_Stream
             // contrastSlider
             // 
             this.contrastSlider.BackColor = System.Drawing.Color.Transparent;
-            this.contrastSlider.Location = new System.Drawing.Point(522, 607);
+            this.contrastSlider.Location = new System.Drawing.Point(328, 643);
             this.contrastSlider.Name = "contrastSlider";
             this.contrastSlider.Size = new System.Drawing.Size(166, 32);
             this.contrastSlider.TabIndex = 3;
@@ -84,24 +85,36 @@ namespace USB_Camera_Stream
             // brightnessLabel
             // 
             this.brightnessLabel.AutoSize = true;
-            this.brightnessLabel.Location = new System.Drawing.Point(283, 620);
+            this.brightnessLabel.Location = new System.Drawing.Point(212, 646);
             this.brightnessLabel.Name = "brightnessLabel";
-            this.brightnessLabel.Size = new System.Drawing.Size(0, 0);
+            this.brightnessLabel.Size = new System.Drawing.Size(71, 19);
             this.brightnessLabel.TabIndex = 4;
+            this.brightnessLabel.Text = "Brightness:";
             // 
             // contrastLabel
             // 
             this.contrastLabel.AutoSize = true;
-            this.contrastLabel.Location = new System.Drawing.Point(744, 620);
+            this.contrastLabel.Location = new System.Drawing.Point(532, 646);
             this.contrastLabel.Name = "contrastLabel";
-            this.contrastLabel.Size = new System.Drawing.Size(0, 0);
+            this.contrastLabel.Size = new System.Drawing.Size(65, 19);
             this.contrastLabel.TabIndex = 5;
+            this.contrastLabel.Text = "Contrast: ";
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(659, 646);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(72, 19);
+            this.metroLabel1.TabIndex = 6;
+            this.metroLabel1.Text = "Resolution:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1089, 672);
+            this.ClientSize = new System.Drawing.Size(934, 702);
+            this.Controls.Add(this.metroLabel1);
             this.Controls.Add(this.contrastLabel);
             this.Controls.Add(this.brightnessLabel);
             this.Controls.Add(this.contrastSlider);
@@ -129,6 +142,7 @@ namespace USB_Camera_Stream
         private MetroFramework.Controls.MetroTrackBar contrastSlider;
         private MetroFramework.Controls.MetroLabel brightnessLabel;
         private MetroFramework.Controls.MetroLabel contrastLabel;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
     }
 }
 

a8c5e31b950f31ade152396def980d1e3f53f873
Sergey Evseev <sevseev@gmail.com>
2023-07-12 01:46:09 +0300

    dark theme for controls

--- USB_Camera_Stream/Form1.Designer.cs
+++ USB_Camera_Stream/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace USB_Camera_Stream
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.resolutionDropdown = new MetroFramework.Controls.MetroComboBox();
@@ -38,7 +39,9 @@ namespace USB_Camera_Stream
             this.brightnessLabel = new MetroFramework.Controls.MetroLabel();
             this.contrastLabel = new MetroFramework.Controls.MetroLabel();
             this.resolutionLabel = new MetroFramework.Controls.MetroLabel();
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -57,66 +60,93 @@ namespace USB_Camera_Stream
             // resolutionDropdown
             // 
             this.resolutionDropdown.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.resolutionDropdown.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.resolutionDropdown.FormattingEnabled = true;
             this.resolutionDropdown.ItemHeight = 23;
-            this.resolutionDropdown.Location = new System.Drawing.Point(663, 578);
+            this.resolutionDropdown.Location = new System.Drawing.Point(663, 565);
             this.resolutionDropdown.Name = "resolutionDropdown";
             this.resolutionDropdown.Size = new System.Drawing.Size(123, 29);
+            this.resolutionDropdown.Style = MetroFramework.MetroColorStyle.Blue;
             this.resolutionDropdown.TabIndex = 1;
+            this.resolutionDropdown.Theme = MetroFramework.MetroThemeStyle.Dark;
             this.resolutionDropdown.UseSelectable = true;
             this.resolutionDropdown.SelectedIndexChanged += new System.EventHandler(this.resolutionDropdown_SelectedIndexChanged);
             // 
             // brightnessSlider
             // 
-            this.brightnessSlider.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.brightnessSlider.BackColor = System.Drawing.Color.Transparent;
-            this.brightnessSlider.Location = new System.Drawing.Point(31, 574);
+            this.brightnessSlider.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.brightnessSlider.BackColor = System.Drawing.Color.DimGray;
+            this.brightnessSlider.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.brightnessSlider.Location = new System.Drawing.Point(31, 565);
             this.brightnessSlider.Name = "brightnessSlider";
-            this.brightnessSlider.Size = new System.Drawing.Size(156, 39);
+            this.brightnessSlider.Size = new System.Drawing.Size(156, 36);
+            this.brightnessSlider.Style = MetroFramework.MetroColorStyle.Blue;
             this.brightnessSlider.TabIndex = 2;
             this.brightnessSlider.Text = "metroTrackBar1";
+            this.brightnessSlider.Theme = MetroFramework.MetroThemeStyle.Dark;
             this.brightnessSlider.ValueChanged += new System.EventHandler(this.brightnessSlider_ValueChanged);
             // 
             // contrastSlider
             // 
             this.contrastSlider.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.contrastSlider.BackColor = System.Drawing.Color.Transparent;
-            this.contrastSlider.Location = new System.Drawing.Point(311, 581);
+            this.contrastSlider.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.contrastSlider.Location = new System.Drawing.Point(303, 569);
             this.contrastSlider.Name = "contrastSlider";
-            this.contrastSlider.Size = new System.Drawing.Size(145, 32);
+            this.contrastSlider.Size = new System.Drawing.Size(157, 32);
+            this.contrastSlider.Style = MetroFramework.MetroColorStyle.Blue;
             this.contrastSlider.TabIndex = 3;
             this.contrastSlider.Text = "metroTrackBar1";
+            this.contrastSlider.Theme = MetroFramework.MetroThemeStyle.Dark;
             this.contrastSlider.ValueChanged += new System.EventHandler(this.contrastSlider_ValueChanged);
             // 
             // brightnessLabel
             // 
+            this.brightnessLabel.AllowDrop = true;
             this.brightnessLabel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.brightnessLabel.AutoSize = true;
-            this.brightnessLabel.Location = new System.Drawing.Point(193, 581);
+            this.brightnessLabel.BackColor = System.Drawing.Color.Gray;
+            this.brightnessLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.brightnessLabel.Location = new System.Drawing.Point(193, 571);
             this.brightnessLabel.Name = "brightnessLabel";
             this.brightnessLabel.Size = new System.Drawing.Size(71, 19);
+            this.brightnessLabel.Style = MetroFramework.MetroColorStyle.Blue;
             this.brightnessLabel.TabIndex = 4;
             this.brightnessLabel.Text = "Brightness:";
+            this.brightnessLabel.Theme = MetroFramework.MetroThemeStyle.Dark;
             // 
             // contrastLabel
             // 
             this.contrastLabel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.contrastLabel.AutoSize = true;
-            this.contrastLabel.Location = new System.Drawing.Point(462, 584);
+            this.contrastLabel.BackColor = System.Drawing.Color.Gray;
+            this.contrastLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.contrastLabel.Location = new System.Drawing.Point(469, 571);
             this.contrastLabel.Name = "contrastLabel";
             this.contrastLabel.Size = new System.Drawing.Size(65, 19);
+            this.contrastLabel.Style = MetroFramework.MetroColorStyle.Blue;
             this.contrastLabel.TabIndex = 5;
             this.contrastLabel.Text = "Contrast: ";
+            this.contrastLabel.Theme = MetroFramework.MetroThemeStyle.Dark;
             // 
             // resolutionLabel
             // 
             this.resolutionLabel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.resolutionLabel.AutoSize = true;
-            this.resolutionLabel.Location = new System.Drawing.Point(585, 584);
+            this.resolutionLabel.BackColor = System.Drawing.Color.Gray;
+            this.resolutionLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.resolutionLabel.Location = new System.Drawing.Point(585, 571);
             this.resolutionLabel.Name = "resolutionLabel";
             this.resolutionLabel.Size = new System.Drawing.Size(72, 19);
+            this.resolutionLabel.Style = MetroFramework.MetroColorStyle.Blue;
             this.resolutionLabel.TabIndex = 6;
             this.resolutionLabel.Text = "Resolution:";
+            this.resolutionLabel.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroStyleManager1
+            // 
+            this.metroStyleManager1.Owner = null;
+            this.metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Dark;
             // 
             // Form1
             // 
@@ -132,10 +162,12 @@ namespace USB_Camera_Stream
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.Theme = MetroFramework.MetroThemeStyle.Default;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Resize += new System.EventHandler(this.Form1_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -152,6 +184,7 @@ namespace USB_Camera_Stream
         private MetroFramework.Controls.MetroLabel brightnessLabel;
         private MetroFramework.Controls.MetroLabel contrastLabel;
         private MetroFramework.Controls.MetroLabel resolutionLabel;
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
     }
 }
 
